82e50a76cd6fe22f2b443c041f0a31b2da0c6472
彭涛 <peengtao123@126.com>
2021-02-20 16:25:04 +0800

    first commit

---
+++ src/compontent/login/index.js
@@ -0,0 +1,13 @@
+import './login.css'
+import React from "react";
+import './login.css'
+
+function Login() {
+    return (
+        <div className="login">
+            <h1>hello world</h1>
+        </div>
+    );
+}
+
+export default Login;
